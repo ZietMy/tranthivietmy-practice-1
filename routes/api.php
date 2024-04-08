f14b72f773d6e367e85f94eb5991b635fc5b78b5
Viet My <Ziettmy@gmail.com>
2024-04-09 02:00:03 +0700

    practice1

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::get('/posts',[PostController::class,'index']);
+Route::get('/posts/{id}',[PostController::class,'show']);
+Route::post('/posts',[PostController::class,'store']);
+Route::delete('/posts/{id}',[PostController::class,'update']);
+Route::put('/posts/{id}',[PostController::class,'destroy']);
