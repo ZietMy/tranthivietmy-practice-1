418a235b39c36d831775d206d02eea860fba5584
Viet My <Ziettmy@gmail.com>
2024-04-09 02:05:20 +0700

    practice2

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,3 +24,9 @@ Route::get('/posts/{id}',[PostController::class,'show']);
 Route::post('/posts',[PostController::class,'store']);
 Route::delete('/posts/{id}',[PostController::class,'update']);
 Route::put('/posts/{id}',[PostController::class,'destroy']);
+
+Route::get('/users',[UserController::class,'index']);
+Route::get('/users/{id}',[UserController::class,'show']);
+Route::post('/users',[UserController::class,'store']);
+Route::delete('/users/{id}',[UserController::class,'update']);
+Route::put('/users/{id}',[UserController::class,'destroy']);
