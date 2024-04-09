95269aa7581fcb8f5ec1b38211b8b937721738fc
Việt Mỹ <137087064+ZietMy@users.noreply.github.com>
2024-04-09 20:21:21 +0700

    practice2 (#1)

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,164 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    /**
+     * @OA\Get(
+     *     path="/api/users",
+     *     summary="Get all users",
+     *     tags={"Users"},
+     *      @OA\Response(response=200, description="All Users" ),
+     *      @OA\Response(response=400, description="Bad request"),
+     *      @OA\Response(response=404, description="Resource Not Found"),
+     *     security={{"bearerAuth":{}}}
+     * )
+     */
+    public function index()
+    {
+        $users = User::all();
+        return $users;
+    }
+    
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    /**
+    * @OA\Post(
+        *     path="/api/users",
+        *     summary="Create a new user",
+        *     description="Create a new user with the provided username, email, and password",
+        *     tags={"Users"},
+        *     @OA\RequestBody(
+        *         required=true,
+        *         @OA\JsonContent(
+        *             required={"name", "email", "password"},
+        *             @OA\Property(property="name", type="string", example="john_ha"),
+        *             @OA\Property(property="email", type="string", format="email", example="johnha@gmail.com"),
+        *             @OA\Property(property="password", type="string", example="password123")
+        *         )
+        *     ),
+        *    @OA\Response(response=200, description="Create New User" ),
+       *     @OA\Response(response=400, description="Bad request"),
+       *      @OA\Response(response=404, description="Resource Not Found"),
+        * )
+        */
+    public function store(Request $request)
+    {
+        return User::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    /**
+     * @OA\Get(
+     *     path="/api/users/{id}",
+     *     summary="Get a specific user",
+     *     tags={"Users"},
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         required=true,
+     *         description="ID of the user to retrieve",
+     *         @OA\Schema(type="integer")
+     *     ),
+     *     @OA\Response(response=200, description="User Detail" ),
+     *      @OA\Response(response=400, description="Bad request"),
+     *      @OA\Response(response=404, description="Resource Not Found"),
+     *     security={{"bearerAuth":{}}}
+     * )
+     */
+    public function show(User $user)
+    {
+        return $user;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+     /**
+     * @OA\Put(
+     *     path="/api/users/{id}",
+     *     summary="Update a specific user",
+     *     tags={"Users"},
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         description="User ID",
+     *         required=true,
+     *         @OA\Schema(type="integer")
+     *     ),
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\MediaType(
+     *             mediaType="application/json",
+     *             @OA\Schema(
+     *                 type="object",
+     *                  @OA\Property(property="name", type="string", example="john_doe"),
+     *             @OA\Property(property="email", type="string", format="email", example="john@example.com"),
+     *             @OA\Property(property="password", type="string", example="password123")
+     *             )
+     *         )
+     *     ),
+     *      @OA\Response(response=200, description="Update user" ),
+    *      @OA\Response(response=400, description="Bad request"),
+    *      @OA\Response(response=404, description="Resource Not Found"),
+    *     security={{"bearerAuth":{}}}
+    * )
+    */
+    public function update(Request $request, User $user)
+    {
+        $user->update($request->all());
+        return $user;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+     /**
+     * @OA\Delete(
+     *     path="/api/users/{id}",
+     *     summary="Delete a specific user",
+     *     tags={"Users"},
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         description="User ID",
+     *         required=true,
+     *         @OA\Schema(type="integer")
+     *     ),
+     *     @OA\Response(response=200, description="Delate user" ),
+     *      @OA\Response(response=400, description="Bad request"),
+     *      @OA\Response(response=404, description="Resource Not Found"),
+     *     security={{"bearerAuth":{}}}
+     * )
+     */
+    public function destroy(User $user)
+    {
+        $user->delete();
+        return "Delete user success";
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -20,4 +20,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::apiResource('posts',PostController::class);
-// Route::apiResource('posts','UserController');
+Route::apiResource('users',UserController::class);
